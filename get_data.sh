899c238f9148ea7c8745aeaf40348493cc020d29
ximhan <ximhan@redhat.com>
2021-07-07 11:57:56 +0800

    update data list

--- get_data.sh
+++ get_data.sh
@@ -3,16 +3,16 @@ then
     http -b http://fund.eastmoney.com/pingzhongdata/$2.js|sed 's/;/\n/g'| grep fS_name |grep -Po '(?<=\").*(?=\")'
 elif [ $1 == "1m" ]
 then
-    curl -s http://fund.eastmoney.com/pingzhongdata/$2.js|sed 's/;/\n/g'| grep syl_1y |grep -Po '(?<=\").*(?=\")'
+    http -b http://fund.eastmoney.com/pingzhongdata/$2.js|sed 's/;/\n/g'| grep syl_1y |grep -Po '(?<=\").*(?=\")'
 elif [ $1 == "3m" ]
 then
-    curl -s http://fund.eastmoney.com/pingzhongdata/$2.js|sed 's/;/\n/g'| grep syl_3y |grep -Po '(?<=\").*(?=\")'
+    http -b http://fund.eastmoney.com/pingzhongdata/$2.js|sed 's/;/\n/g'| grep syl_3y |grep -Po '(?<=\").*(?=\")'
 elif [ $1 == "6m" ]
 then
-    curl -s http://fund.eastmoney.com/pingzhongdata/$2.js|sed 's/;/\n/g'| grep syl_6y |grep -Po '(?<=\").*(?=\")'
+    http -b http://fund.eastmoney.com/pingzhongdata/$2.js|sed 's/;/\n/g'| grep syl_6y |grep -Po '(?<=\").*(?=\")'
 elif [ $1 == "stocklist" ]
 then
-    curl -s http://fund.eastmoney.com/pingzhongdata/$2.js|sed 's/;/\n/g'|grep stockCodesNew|grep -Po '(?<=\[).*(?=\])'|sed 's/,/\n/g'|cut -d '.' -f 2|sed 's/"//g'
+    http -b http://fund.eastmoney.com/pingzhongdata/$2.js|sed 's/;/\n/g'|grep stockCodesNew|grep -Po '(?<=\[).*(?=\])'|sed 's/,/\n/g'|cut -d '.' -f 2|sed 's/"//g'
 elif [ $1 == "getstock" ]
 then
     cat stock_list.txt | grep $2
